f24694db5fcfb8857797d4eacdc6a87e7840f474
Darren Suroviec <dcsuroviec@gmail.com>
2020-07-29 14:42:48 -0700

    Widgets app update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 const items = [
   {
@@ -17,10 +19,27 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: "a color of red",
+    value: "red",
+  },
+
+  {
+    label: "a color of green",
+    value: "green",
+  },
+
+  {
+    label: "a shade of blue",
+    value: "blue",
+  },
+];
+
 export default () => {
   return (
     <div>
-      <Search />
+      <Translate />
     </div>
   );
 };
